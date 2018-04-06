48035f4e8c1f292907860461be370b1974834310
davednguyen <davednguyen@gmail.com>
2018-04-05 19:32:12 -0700

    updates

--- script.js
+++ script.js
@@ -20,78 +20,94 @@ var courseList = [];
 // REVIEW: Would this be safer if variable was scoped to window.onload or addACourse functions?
 
 // Assign handlers to page events.
-window.onload = function(){
-  document.querySelector("form#add-course").addEventListener("submit", addACourse)
+window.onload = function() {
+    document.querySelector("form#add-course").addEventListener("submit", addACourse);
 
-  document.querySelector("button#clear").addEventListener("click", clearData)
+    document.querySelector("button#clear").addEventListener("click", clearData);
 
-  document.querySelector("button#calculate").addEventListener("click", calculateAverage)
-  // REVIEW: Would it make more sense to make some handlers anonymous functions,
-  // instead of defining them elsewhere and assigning them here?
+    document.querySelector("button#calculate").addEventListener("click", calculateAverage);
+    // REVIEW: Would it make more sense to make some handlers anonymous functions,
+    // instead of defining them elsewhere and assigning them here?
 }
 
 // Triggered on form submit. Creates a new course object and pushes it into courseList array,
 // clears content in form fields, prints courseList objects to the page.
 // BUGFIX: This function isn't working properly- nothing gets output to the list on form submit
-function addACourse(){
-  var grade = parseFloat(this.elements["grade"].value);
-  /*
-    TODO: validate that "grade" value is a number between 1.0 and 4.0, stop processing if it is not.
+function addACourse() {
+    event.preventDefault();
+    var grade = parseFloat(this.elements["grade"].value);
+    /*
+      TODO: validate that "grade" value is a number between 1.0 and 4.0, stop processing if it is not.
 
-    Checking 'grade typeof "number"' will always return true because we called parseFloat.
-    We must instead check that it's value is not NaN.
+      Checking 'grade typeof "number"' will always return true because we called parseFloat.
+      We must instead check that it's value is not NaN.
 
-    REVIEW: could we make it doubly safe by adding an HTML validation as well?
-  */
+      REVIEW: could we make it doubly safe by adding an HTML validation as well?
+    */
 
-  // Create the new course with values from the form, push it into array of courses.
-  var newCourse = {
-    name: this.elements["name"].value,
-    grade: grade
-  }
+    // Create the new course with values from the form, push it into array of courses.
+    var newCourse = {
+        name: this.elements["name"].value,
+        grade: grade
+    };
 
-  courseList.push(newCourse)
+    console.log(newCourse);
+    courseList.push(newCourse);
+    console.log(courseList);
+    var gradecount = document.getElementById("course-list");
+    gradecount = courseList.length;
 
-  clearFormFields()
-  outputList()
+    clearFormFields()
+    outputList()
 }
 
 // Calculate the average of "grade" attribute for all objects in courseList array
 // and print it in friendly message to page text.
-function calculateAverage(){
-  // Average GPA of courses equals the sum of all grades,
-  // divided by the number of course objects in courseList
-  var sum = 0
-  for(var i=0;i<courseList.length;i++){
-    sum += courseList[i].grade
-  }
-  var avg = sum / courseList.length
-  document.getElementById("result").innerHTML = "Your overall GPA is " + avg;
+function calculateAverage() {
+    // Average GPA of courses equals the sum of all grades,
+    // divided by the number of course objects in courseList
+    var sum = 0
+    for (var i = 0; i < courseList.length; i++) {
+        sum += courseList[i].grade
+    }
+    var avg = sum / courseList.length
+    document.getElementById("result").innerHTML = "Your overall GPA is " + avg;
 }
 
 // Removes GPA calculation from page text.
-function clearGPA(){
-  document.getElementById("result").innerHTML = null;
+function clearGPA() {
+    document.getElementById("result").innerHTML = null;
 }
 
 // Clears content in form fields.
-function clearFormFields(){
-  // TODO: implement this function.
+function clearFormFields() {
+    // TODO: implement this function.
 }
 
 // Clear out list of courses and all content shown on the page
-function clearData(){
-  courseList = []
-  clearFormFields()
-  outputList()
+function clearData() {
+    courseList = []
+    clearFormFields()
+    outputList()
 }
 
 // Prints courseList objects to the page in a readable way.
-function outputList(){
-  var list = document.getElementById("course-list");
-  /*
-    TODO: Clear the existing contents of the "list" element. Then, for each object in courseList,
-    create an li element that holds the course's name and grade, and append
-    it to the "list" ul element.
-  */
+function outputList() {
+    var list = document.getElementById("course-list");
+    list.innerHTML = "";
+    console.log("list: " + list);
+
+
+    for (var i = 0; i < courseList.length; i++) {
+        var info = ("name: " + courseList[i].name + " grade: " + courseList[i].grade);
+        console.log(info);
+        var node = document.createElement("li");
+        node.innerHTML = info;
+        list.appendChild(node);
+    }
+    /*
+      TODO: Clear the existing contents of the "list" element. Then, for each object in courseList,
+      create an li element that holds the course's name and grade, and append
+      it to the "list" ul element.
+    */
 }
